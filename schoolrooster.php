8d832afc9a698efa7d47f86f867c3c7e19d4b64e
hagger58 <44053720+hagger58@users.noreply.github.com>
2018-10-18 17:05:25 +0200

    Add files via upload

---
+++ schoolrooster.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+
+<!-- De locatie is: <?php echo $_POST["locatie"];?><br>
+Het locatieadres is: <?php echo $_POST["locatieadres"];?>
+De locatie postcode is: <?php echo $_POST["locatiepostcode"];?>
+
+Het lokaal-nummer is: <?php echo $_POST["lokaalnr"];?><br>
+De etage is: <?php echo $_POST["etage"];?>
+De locatie is: <?php echo $_POST["locatie"];?>
+
+De naam van het vak is: <?php echo $_POST["vaknaam"];?><br>
+
+Het niveau is: <?php echo $_POST["nivo"];?><br> -->
+
+Het klas-nummer is: <?php echo $_POST["klassen"];?><br>
+Het niveau is: <?php echo $_POST["niveau"];?>
+
+<!--De voornaam van de leraar is: <?php echo $_POST["voornaam"];?><br>
+De achternaam van de leraar is: <?php echo $_POST["achternaam"];?>
+De geboortedatum van de leraar is: <?php echo $_POST["geb_dat"];?>
+-->
+</body>
+<?php
+// header("Location: http://localhost/PHP_code/schoolrooster/index.php")
+?>
+</html>
